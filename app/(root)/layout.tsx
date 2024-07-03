a4d7acf84d2c72a2ca4ca2445306f01c85e8e890
Olaleye Fuad <heniolarr@gmail.com>
2024-07-03 15:55:21 +0100

    Update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,6 @@
 
 'use client'
 
-import { createContext, useContext, useState } from 'react';
 
 import SideNav from "@/components/ui/SideNav";
 import NavBar from '@/components/NavBar'
@@ -10,7 +9,6 @@ import { Barlow, Inter } from "next/font/google";
 const barlow = Barlow({weight:['100', '200', '300', '400', '500'] ,subsets:['latin']})
 const inter = Inter({subsets: ['latin']})
 
-const userContext = createContext();
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>) 
 
@@ -18,24 +16,14 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
 
 {
 
-  const [currentUser, setCurrentUser] = useState({})
-
-  const fetchUser = async () => {
-    const res = await fetch('/api/fetch_user')
-    const data = await res.json()
-    setCurrentUser(data)
-  }
-
 
   return (
     <html lang="en">
       <body className="flex flex-row pl-[22vw]">
         <SideNav />
         <div className={`${inter.className} w-full bg-gray px-8 py-4 min-h-screen`}>
-          <userContext.Provider value={{ currentUser, fetchUser }}>
             <NavBar />
             {children}
-          </userContext.Provider>
         </div>
       </body>
     </html>
