9cea46c24e4242de61d2c083c1e6326a8e26da91
Olaleye Fuad <heniolarr@gmail.com>
2024-07-03 11:43:26 +0100

    Migrated to jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,19 +1,16 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata= {
   title: "Cargo-run Admin",
   description: "Admin Dashboard for Cargo-run",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body>
